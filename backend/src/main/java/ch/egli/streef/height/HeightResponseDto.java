78616db4d3bfb9ea207de3769f69bd98acd4f634
Simon Egli <simon.e98zh@gmail.com>
2025-05-13 18:38:05 +0200

    remove backend

--- backend/src/main/java/ch/egli/streef/height/HeightResponseDto.java
+++
@@ -1,5 +0,0 @@
-package ch.egli.streef.height;
-
-// Representation of Response from Endpoint "https://api3.geo.admin.ch/services/sdiservices.html#height"
-public record HeightResponseDto(double height) {
-}
--- backend/src/main/java/ch/egli/streef/trackpoint/TrackPointRepository.java
+++
@@ -1,6 +0,0 @@
-package ch.egli.streef.trackpoint;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrackPointRepository extends JpaRepository<TrackPointEntity, Long> {
-}
