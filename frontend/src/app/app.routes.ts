79d5e560529be0f9441332181351e2e04640123c
Simon Egli <simon.e98zh@gmail.com>
2025-05-13 20:17:26 +0200

    add basic frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TrackComponent } from './track/track.component';
+import { FeedComponent } from './feed/feed.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'feed', component: FeedComponent },
+    { path: 'track', component: TrackComponent },
+    { path: '', redirectTo: 'feed', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
---
+++ frontend/src/app/track/track.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track',
+  imports: [],
+  templateUrl: './track.component.html'
+})
+export class TrackComponent {
+  constructor() {
+    console.log('Hello');
+    
+  }
+
+}
