f81f491b54881526ab6c350d8f322da618a302d6
Simon Egli <simon.e98zh@gmail.com>
2025-05-13 21:26:35 +0200

    improve handling of parameter

--- frontend/src/app/track/track.component.ts
+++ frontend/src/app/track/track.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { inject } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, Input, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TrackDto } from './track.dto';
 import { AsyncPipe } from '@angular/common';
@@ -12,20 +10,19 @@ import { TrackService } from './track-service';
   templateUrl: './track.component.html'
 })
 export class TrackComponent {
-  private readonly route = inject(ActivatedRoute);
-  private readonly router = inject(Router);
   private readonly trackService = inject(TrackService)
-
   track$!: Observable<TrackDto>;
 
-  constructor() {
-    // TODO Handle garbage params
-    const trackId = this.route.snapshot.paramMap.get('id');
-    if (trackId) {
-      this.track$ = this.trackService.byId(+trackId);
-    } else {
-      // TODO
-      console.error('Handle me')
+  @Input()
+  set id(trackId: number) {
+    const parsedId = Number(trackId);
+
+    if (!Number.isInteger(parsedId) || parsedId <= 0) {
+      console.error(`Invalid track ID:`, trackId);
+      // TODO: what to do?
+      return;
     }
+
+    this.track$ = this.trackService.byId(trackId);
   }
 }
