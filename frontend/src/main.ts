4622650d1c66ab38acd12838ea05a0969ec43af1
Simon Egli <simon.e98zh@gmail.com>
2025-05-15 07:30:38 +0200

    add comment

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,5 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// TODO: Move project to UI dir in Spring?: https://github.com/spring-guides/tut-spring-security-and-angular-js/tree/main/oauth2
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
